bc41d7fedb9ad5acaf1b6ad50ba18033bf48a9a2
ignaciocervino <ignaciocervino@gmail.com>
2021-06-27 23:18:30 -0300

    #05 Creating Budget class and a instance of it

--- js/app.js
+++ js/app.js
@@ -13,12 +13,26 @@ function eventListeners(){
 
 
 /** Clases */
+class Presupuesto{
+    constructor(presupuesto){
+        this.presupuesto = Number(presupuesto);
+        this.restante = Number(presupuesto);//Porque el restante es el mismo al presupuesto al iniciar
+        this.gastos = []
+    }
+}
 
+class UI{
 
+}
+
+//Instanciar globalmente
+const ui = new UI();
+let presupuesto;
 /** Funciones */
 function preguntarPresupuesto(){
     const presupuestoUsuario = prompt('¿Cual es tu presupuesto?');
     if(presupuestoUsuario === '' || presupuestoUsuario===null || isNaN(presupuestoUsuario) || presupuestoUsuario<=0){//Cuando da cancelar es null
         window.location.reload();//Recarga la ventana actual
     }
+    presupuesto = new Presupuesto(presupuestoUsuario);
 }
